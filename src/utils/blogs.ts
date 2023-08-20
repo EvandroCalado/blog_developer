5529ae6e8241ef1dc6c17940942a56d11479e362
evandrocalado <evandrocalado07@gmail.com>
2023-08-20 17:13:05 -0300

    create mocks for stories styling

---
+++ src/utils/blogs.ts
@@ -0,0 +1,80 @@
+export const blogs = [
+  {
+    id: '64de0ba4bc4421dffafb367712',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+  {
+    id: '64de0ba4bc4421dffafb367723',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+  {
+    id: '64de0ba4bc4421dffafb367745',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+  {
+    id: '64de0ba4bc4421dffafb367756',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+  {
+    id: '64de0ba4bc4421dffafb367778',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+  {
+    id: '64de0ba4bc4421dffafb36777854',
+    title: 'NextJs is awesome',
+    description:
+      '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis cupiditate culpa corporis nulla placeat aliquid? Autem perferendis possimus quasi facere cum fugit, totam ducimus excepturi hic nobis quis, unde animi.</p>',
+    imageUrl:
+      'http://res.cloudinary.com/dnhsdenji/image/upload/v1692273570/blog_dev_evandro/dazaggeltsjxuofgsopc.jpg',
+    userId: '64dcc7683b60e2f50838b332',
+    createdAt: '2023-08-17T11:59:32.874Z',
+    updatedAt: '2023-08-17T11:59:32.874Z',
+    categoryId: '64dd8fb10928bb143a6d046e',
+    location: 'Brasil',
+  },
+];
---
+++ src/utils/categories.ts
@@ -0,0 +1,31 @@
+export const categories = [
+    {
+      id: '64dd8fb10928bb143a6d046e',
+      name: 'Next',
+    },
+    {
+      id: '64dd8fb90928bb143a6d0470',
+      name: 'React',
+    },
+    {
+      id: '64e0032dda343fc535c796ad',
+      name: 'Javascript',
+    },
+    {
+      id: '64e00339da343fc535c796af',
+      name: 'Typescript',
+    },
+    {
+      id: '64e0037fda343fc535c796b2',
+      name: 'Prisma',
+    },
+    {
+      id: '64e00390da343fc535c796b4',
+      name: 'MongoDb',
+    },
+    {
+      id: '64e0039cda343fc535c796b6',
+      name: 'Postegres',
+    },
+  ];
+  
