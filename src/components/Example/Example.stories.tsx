f91f70e526b83eb1efa068301f3837083d5ae767
evandrocalado <evandrocalado07@gmail.com>
2023-08-20 17:16:35 -0300

    remove Example component

--- src/components/Example/Example.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Example from './Example';
-import { Meta } from '@storybook/react';
-
-export default {
-  title: 'Example',
-  component: Example,
-} as Meta;
-
-export const Default = () => <Example />;
